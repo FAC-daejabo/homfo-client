8826d7f416297bee4835e8f556b244bee82f2de7
yunmi099 <yunmi099@ajou.ac.kr>
2024-04-29 16:14:41 +0900

    프로젝트 생성 및 개발환경 세팅

---
+++ packages/mobile-user/.eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  extends: [
+    '@react-native',
+    'airbnb',
+    'prettier',
+    'prettier/react',
+    'plugin:prettier/recommended',
+    'eslint-config-prettier',
+  ],
+  parser: 'babel-eslint',
+  rules: {
+    'no-unused-vars': 'off',
+    'import/no-unresolved': 'off',
+    'react/jsx-filename-extension': [
+      1,
+      {
+        extensions: ['.js', '.jsx'],
+      },
+    ],
+    'prettier/prettier': [
+      'error',
+      {
+        trailingComma: 'es5',
+        singleQuote: true,
+        printWidth: 100,
+      },
+    ],
+  },
+  plugins: ['prettier'],
+};
---
+++ packages/web-admin/src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+import { Button } from '@homfo-client/ui';
+function App() {
+  return (
+    <>
+      <Button/>
+    </>
+  )
+}
+
+export default App
